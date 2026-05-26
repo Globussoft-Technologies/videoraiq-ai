8a5e665f73df4901f43fac8304dbdbe86c3f6508
Afzal A <afzal.a@globussoft.in>
2026-05-26 13:07:44 +0530

    Revert "fix(permissions): case-insensitive module name matching in bulkPermissionDelete"
    This reverts commit bd56f9df1ccb5c4005b1aace745ecdd1df89b78c.

--- server/core/v1/permission/permissions.utility.js
+++ server/core/v1/permission/permissions.utility.js
@@ -466,23 +466,11 @@ class PermissionService {
                 return res.send(Response.FailResp(PermissionMessageNew["NO_PERMISSION_CONFIG_PROVIDED"]['en'], "error"));
             }
     
-            let moduleNamesToDelete = data.permissionConfig.map(item => item.moduleName);
-
+            let moduleNamesToDelete = data.permissionConfig.map(item => item.moduleName.toLowerCase());
+    
             let adminPermission = await permissionModel.findOne({ adminId: adminId});
 
-            // Case-insensitive module lookup: map requested module names to actual keys in permissionConfig
-            const configKeys = Object.keys(adminPermission.permissionConfig || {});
-            const moduleMap = {};
-            const missingModules = [];
-
-            moduleNamesToDelete.forEach(requestedModule => {
-                const actualKey = configKeys.find(key => key.toLowerCase() === requestedModule.toLowerCase());
-                if (actualKey) {
-                    moduleMap[requestedModule] = actualKey;
-                } else {
-                    missingModules.push(requestedModule);
-                }
-            });
+            let missingModules = moduleNamesToDelete.filter(moduleName => !adminPermission.permissionConfig.hasOwnProperty(moduleName));
 
             if (missingModules.length > 0) {
                 // Return error if any moduleName is not found in the database
@@ -490,9 +478,8 @@ class PermissionService {
             }
 
             // Construct an object where each key represents a module to be removed from permissionConfig
-            let unsetFields = moduleNamesToDelete.reduce((acc, requestedModule) => {
-                const actualKey = moduleMap[requestedModule];
-                acc[`permissionConfig.${actualKey}`] = "";
+            let unsetFields = moduleNamesToDelete.reduce((acc, moduleName) => {
+                acc[`permissionConfig.${moduleName}`] = "";
                 return acc;
             }, {});
 
